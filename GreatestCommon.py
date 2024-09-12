5f318e91d975eafed50075b764482a164ee3dd2f
andrkrsv <andrkrsv@gmail.com>
2024-09-12 09:39:55 +0300

    алгоритм(не работает вывод)

---
+++ GreatestCommon.py
@@ -0,0 +1,10 @@
+def greatestcommondivisor(number):
+    number = number << 0
+    GCD = 1
+    while(number&1==0):
+        number >> 1 = number 
+        print(f"DEBUG: Number = {number}")
+        GCD << 1 = GCD
+        print(f"DEBUG: GCD = {GCD}")
+    return GCD
+    
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-#wcdscsc!
+import GreatestCommon
+
+print("Задание 1 \n")
+print(GreatestCommon.greatestcommondivisor(int(input("Напишите число: "))))
