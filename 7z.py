80e0cd987d16800e28c924e74d9d0263c57703d5
andrkrsv <andrkrsv@gmail.com>
2024-09-15 22:01:14 +0300

    я выводил обьект генератора)))

--- 7z.py
+++ 7z.py
@@ -1,21 +1,28 @@
 #zadanie 7 chisla fibonachi
 
-debug = True
+debug = False
 
 def fibonachi_numbers(iterations):
     a = 0
     b = 1
     unfull_summ = 0
 
-    for _ in range(iterations):
-        if debug: print("iteration: ", iterations)
-        unfull_summ += a
-        if debug: print("unfullsumm: ", unfull_summ)
-        yield unfull_summ
-        if debug: print("unfullsumm: ", unfull_summ)
+    if iterations <= 20575: #итоговое число выходит за инт, если выполнить такое колво итераций
+        for i in range(iterations):
+            unfull_summ += a
+            if debug: print("unfullsumm: ", unfull_summ)
+            yield unfull_summ
+            if debug: print("unfullsumm: ", unfull_summ)
+            if debug: print("a and b before: ", a, " ", b)
+            a ,b = b, a + b
+            if debug: print("a and b after: ", a, " ", b)
+            if debug: print("iteration: ", i)
+    else:
+        print("Слишком большое количество итераций, превышен лимит Int, максимальное значение: 20575")
 
-        a = b
-        b = a + b
 
+generator = fibonachi_numbers(int(input("введите число итераций: ")))
 
-print(fibonachi_numbers(int(input("введите число итераций: "))))
+for n in generator:
+    print(f"Сумма: {n}")
+
