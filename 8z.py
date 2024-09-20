a61a42ad71fa8081e46e306f4a43b41bd70c3475
andrkrsv <andrkrsv@gmail.com>
2024-09-20 22:47:40 +0300

    8z

--- 8z.py
+++ 8z.py
@@ -1,5 +1,3 @@
-#zadanie 8 chisla fibonachi
-
 debug = False
 
 def fibonachi_numbers(iterations):
@@ -7,22 +5,30 @@ def fibonachi_numbers(iterations):
     b = 1
     unfull_summ = 0
 
-    if iterations <= 20575: #итоговое число выходит за инт, если выполнить такое колво итераций
+    if iterations <= 20575: 
         for i in range(iterations):
             unfull_summ += a
             if debug: print("unfullsumm: ", unfull_summ)
             yield unfull_summ
             if debug: print("unfullsumm: ", unfull_summ)
             if debug: print("a and b before: ", a, " ", b)
-            a ,b = b, a + b
+            a, b = b, a + b
             if debug: print("a and b after: ", a, " ", b)
             if debug: print("iteration: ", i)
     else:
         print("Слишком большое количество итераций, превышен лимит Int, максимальное значение: 20575")
 
 
-generator = fibonachi_numbers(int(input("введите число итераций: ")))
+iterations = int(input("введите число итераций: "))
+min_digits = int(input("Введите минимальное количество значащих цифр: "))
+
+
+generator = fibonachi_numbers(iterations)
+
+index = 0
 
 for n in generator:
-    print(f"Сумма: {n}")
-
+    if len(str(n)) > min_digits:
+        print(f"Первое число с более чем {min_digits} цифрами: {n}, на индексе: {index}")
+        break 
+    index += 1 
